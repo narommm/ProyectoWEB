727da5ab1633d8d1d3291f1c651c9e098670a981
MoisesRosales <00218916@uca.edu.sv>
2019-11-16 15:56:08 -0600

    Add login

---
+++ login.php
@@ -0,0 +1,140 @@
+<?php
+require("conexion.php");
+session_start();
+
+if (isset($_SESSION['nombre'])) {
+         header('location: index.php');
+    exit();
+    }
+?>
+<?php
+if(!empty($_POST)) {
+
+    $usuario = $_POST["usuario"];
+    $pass = md5($_POST["contra"]);
+
+        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM USUARIO WHERE username = ? AND pass =?";
+        $stmt =$PDO->prepare($sql);
+        $stmt->execute(array($usuario, $pass));
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        $PDO = null;
+        if(!empty($data)) {
+
+            //Almacenamos el nombre de usuario en una variable de sesión usuario
+            $_SESSION['usuario'] = $data['username'];
+            $_SESSION['nombre'] = $data['nombre'];
+            $_SESSION['tipo'] = $data['tipo'];
+            
+              echo($_SESSION['usuario']);
+              echo($_SESSION['nombre']);
+              echo($_SESSION['tipo']);
+
+              header('location: index.php');
+          }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Cyrus Studio</title>
+
+<!-- Google fonts -->
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+
+<!-- font awesome -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/animate/animate.css" />
+<link rel="stylesheet" href="assets/animate/set.css" />
+
+<!-- gallery -->
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+<!-- favicon -->
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+
+<link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body>
+<div id="contact" class="spacer">
+
+<!--Login Starts-->
+
+<form class="container contactform center" role="form" method ='POST'>
+<h2 class="text-center  wowload fadeInUp">Start your session</h2>
+  <form class="row wowload fadeInLeftBig">      
+      <div class="col-sm-6 col-sm-offset-3 col-xs-12">      
+        <input type="text" placeholder="username" id="usuario" name="usuario">
+        <input type="password" placeholder="password" id="contra" name="contra">
+        <button class="btn btn-primary"><i class="fa fa-paper-plane" type="submit"></i> Sign In</button>
+      </div>
+  </form>
+
+
+
+</form>
+</div>
+<!--Login Ends-->
+
+
+
+<!-- Footer Starts -->
+<div class="footer text-center spacer">
+<p class="wowload flipInX"><a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-instagram fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-flickr fa-2x"></i></a> </p>
+Copyright 2014 Cyrus Creative Studio. All rights reserved.
+</div>
+<!-- # Footer Ends -->
+<a href="#home" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
+
+
+
+
+
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title">Title</h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->    
+</div>
+
+
+
+<!-- jquery -->
+<script src="assets/jquery.js"></script>
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+<script src="assets/respond/respond.js"></script>
+
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+</body>
+</html>
