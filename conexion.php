52abc9abe333e8b35fdef2a6cc4451539ab17a7d
narommm <00113417@uca.edu.sv>
2019-11-16 16:02:27 -0600

    Nueva Conexion

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php
-    $host = 'localhost';
-    $user = 'admin';
-    $pass = 'root';
-    $dbname = 'ProyectoWeb';
+    $host = 'raja.db.elephantsql.com';
+    $user = 'npyottjk';
+    $pass = 'MOplwc_adGR6KKJ9NCQ5vZ8QRBN960Wd';
+    $dbname = 'npyottjk';
     try
     {
-        $PDO = new PDO("mysql:host=".$host."; "."dbname=".$dbname, $user, $pass);
+        $PDO = new PDO("pgsql:host=".$host."; "."dbname=".$dbname, $user, $pass);
+        echo "Conectado";
     }
     catch(PDOException $e)
     {
