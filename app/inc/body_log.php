c5ea0564a5835e046f96381886f3eaa5ef7893d0
MoisesRosales <00218916@uca.edu.sv>
2019-11-19 15:17:01 -0600

    Woeking

--- app/inc/body_log.php
+++ app/inc/body_log.php
@@ -1,4 +1,4 @@
-<body>
+  <body>
 <div class="topbar animated fadeInLeftBig"></div>
 
 <!-- Header Starts -->
@@ -246,19 +246,6 @@
 <!--Contact Ends-->
 
 
-
-<!-- Footer Starts -->
-<div class="footer text-center spacer">
-<p class="wowload flipInX"><a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-instagram fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-flickr fa-2x"></i></a> </p>
-Copyright 2014 Cyrus Creative Studio. All rights reserved.
-</div>
-<!-- # Footer Ends -->
-<a href="#home" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
-
-
-
-
-
 <!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
     <!-- The container for the modal slides -->
@@ -270,9 +257,9 @@ Copyright 2014 Cyrus Creative Studio. All rights reserved.
     <a class="close">×</a>
     <!-- The modal dialog, which will be used to wrap the lightbox content -->    
 </div>
-
-
-
+<?php
+  include 'app/inc/footer.php';
+?>
 <!-- jquery -->
 <script src="assets/jquery.js"></script>
 
