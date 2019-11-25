fff2391dfa87c150f19f63b4da236c21827915ce
wilber65218 <00065218@uca.edu.sv>
2019-11-25 09:57:29 -0600

    conexion.php comentado

--- conexion.php
+++ conexion.php
@@ -1,14 +1,17 @@
 <?php
+/* implementandocaractetistica de la base datos tales como link, usuario, contraseña, y nombre de la base*/
     $host = 'raja.db.elephantsql.com';
     $user = 'npyottjk';
     $pass = 'MOplwc_adGR6KKJ9NCQ5vZ8QRBN960Wd';
     $dbname = 'npyottjk';
     try
     {
+        /* instanciando el arranque de la base con pgadmin y enviando parametros de inicialización */
         $PDO = new PDO("pgsql:host=".$host."; "."dbname=".$dbname, $user, $pass);
     }
     catch(PDOException $e)
     {
+        /* si ocurre un error al conectar, se envia un mensaje de error */
         die($e->getMessage());
     }
 ?>
