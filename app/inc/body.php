d7b447831f78c18d8cac8a7ce02a99a7d9983362
wilber65218 <00065218@uca.edu.sv>
2019-11-26 21:06:09 -0600

    body.php

--- app/inc/body.php
+++ app/inc/body.php
@@ -1,25 +1,30 @@
 <body>
 <div class="topbar animated fadeInLeftBig"></div>
 
-<!-- Header Starts -->
+<!-- Header iniciado -->
 <div class="navbar-wrapper">
       <div class="container">
         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="top-nav">
+          <!-- creando contenedor -->
           <div class="container">
+          <!-- declarando cabecera del contenedor conuna clase header -->
             <div class="navbar-header">
-              <!-- Logo Starts -->
+              <!-- Iniciando Logo en index -->
               <a class="navbar-brand" href="index.php"><img src="images/LOGO1.png" alt="logo"></a>
-              <!-- #Logo Ends -->
+              <!-- creacion de boton  -->
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <!-- creando spans, como acomodadores oara los iconos -->
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
             </div>
-            <!-- Nav Starts -->
+            <!-- iniciando spans -->
             <div class="navbar-collapse  collapse">
+            <!-- iniciando nav para menú -->
               <ul class="nav navbar-nav navbar-right">
+              <!-- menu representado con inicio, sobre, calenadrio, reserva, salir -->
                  <li class="active"><a href="#home">Home</a></li>
                  <li ><a href="#about">About</a></li>
                  <li ><a href="app/jqueryCalendar/viewCalendar.php">Calendar</a></li>
@@ -27,59 +32,67 @@
                  <li><a href="login.php">Login</a></li>
               </ul>
             </div>
-            <!-- #Nav Ends -->
+            <!-- cerrando nav -->
           </div>
         </div>
       </div>
     </div>
-<!-- #Header Starts -->
+<!-- iniciando header -->
 
 
 
 
 <div id="home">
-<!-- Slider Starts -->
+<!-- iniciando carouser de imagenes y la animación -->
   <div id="myCarousel" class="carousel slide banner-slider animated bounceInDown" data-ride="carousel">     
         <div class="carousel-inner">
-          <!-- Item 1 -->
+          <!-- Añadiendo imagen 1 -->
           <div class="item active">
             <img src="images/BANNER1.jpg" alt="banner">
           </div>
-          <!-- #Item 1 -->
-          <!-- Item 1 -->
+
+           <!--Añadiendo imagen 2 -->
           <div class="item">
             <img src="images/BANNER2.jpg" alt="banner">
           </div>
-          <!-- #Item 1 -->
-          <!-- Item 1 -->
+
+         <!-- Añadiendo imagen 3 -->
           <div class="item">
             <img src="images/BACK3.jpg" alt="banner">
           </div>
-          <!-- #Item 1 -->
+          
         </div>
+        <!-- Configurando links hacia carousel mediante botones laterales -->
         <<a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon-chevron-left"><i class="fa fa-angle-left"></i></span></a>
         <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon-chevron-right"><i class="fa fa-angle-right"></i></span></a>
       </div>
-<!-- #Slider Ends -->
+<!-- finalizando animacion -->
 </div>
-<!-- Cirlce Starts -->
+<!-- iniciando el about con un contenedor -->
 <div id="about"  class="container spacer about">
+<!-- Añadiendo un texto como tema a nuestro contenedor -->
 <h2 class="text-center wowload fadeInUp">Sistema de reserva de laboratorios.</h2>  
   <div class="row">
   <div class="col-sm-6 wowload fadeInLeft">
+  <!-- Especificando aspectos de relevancia a nuestra información -->
     <h4><i class="fa fa-flag"></i> Introducción</h4>
+      <!-- Montando un parrafo  -->
     <p>Bienvenido al sistema de reservas de la universidad José Simeón Cañas (UCA) en el cual podrá solicitar el uso de nuestras instalaciones. Contamos con # laboratorios a su disposición cuyos horarios hábiles son de lunes a  viernes desde las 7:00 am hasta las 7:00 pm. Además de días sábados de 7:00 am a 3:00 pm.</p>
     
   </div>
+   <!-- Agregando un nuevo div a la par de nuetra intro, con información importante-->
   <div class="col-sm-6 wowload fadeInRight">
   <h4><i class="fa fa-fire"></i></i> Importante.</h4>
+  <!-- Agregando parrafo de información relevante -->
   <p>La Universidad José Simeón Cañas se reserva los derechos de admisión de agentes externos a nuestro sistema, por lo que estos deberán contactar a nuestro administrador de servicios solicitándole credenciales de uso del sistema.</p>    
   </div>
   </div>
+  <!-- Abiendo nueva sección de, servicios, la cual contendra las reservas -->
   <div class="services">
   <h3 class="text-center wowload fadeInUp">Servicios</h3>
 	<ul class="row text-center list-inline  wowload bounceInUp">
         <li>
+            <!-- agregando link de reserva -->
             <span><i class="fa fa-cube"></i><b></b>ICPC</span>
         </li>
         <li>
@@ -91,34 +104,47 @@
   	</ul>
   </div>
 </div>
-<!-- #Cirlce Ends -->
+<!-- finalizando divs -->
 
 <br>
 <br>
 <br>
-<!-- works -->
+<!-- iniciando vid contenedor -->
 <div id="calendar"  class=" clearfix grid"> 
     <figure class="effect-oscar  wowload fadeInUp">
+    <!-- añadiendo imagenes alusivas a laboratorios, para promoción llamativa de interés -->
           <img src="images/pictures/icpc.jpg" alt="img01"/>
         <figcaption>
+        <!-- nombrando a a la competencia ICPC como acontecimiento importante -->
             <h2>ICPC</h2>
             <p>International Competition<br>
+            <!-- Añadiendo una sección de ver más para proporcionar información -->
             <a href="images/pictures/icpc.jpg" title="F" data-gallery>Ver mas</a></p>     
         </figcaption>
+           <!-- finalizando la primera animación -->
     </figure>
+    <!-- iniciando nueva animación de proxima imagen -->
      <figure class="effect-oscar  wowload fadeInUp">
+     <!-- añadiendo la inserción de nuestra imagen -->
         <img src="images/portfolio/2.jpg" alt="img01"/>
         <figcaption>
+        <!-- nombrando la secunda asección de nuestro contenedor -->
             <h2>Febrero</h2>
             <p>Lily likes to play with crayons and pencils<br>
+              <!-- Añadiendo una opción de ver más  -->
             <a href="images/portfolio/2.jpg" title="1" data-gallery>Ver mas</a></p>            
         </figcaption>
     </figure>
+    <!-- iniciando proxima sección -->
      <figure class="effect-oscar  wowload fadeInUp">
+     <!-- añadiendo imagen -->
         <img src="images/portfolio/3.jpg" alt="img01"/>
         <figcaption>
+        <!-- nombando a nuestra sección -->
             <h2>music</h2>
+            <!-- insertando un verso -->
             <p>Lily likes to play with crayons and pencils<br>
+            <!-- ver mas como una opción  -->
             <a href="images/portfolio/3.jpg" title="1" data-gallery>Ver mas</a></p>            
         </figcaption>
     </figure>
@@ -127,11 +153,13 @@
 </div>
 </div>
 <div id="contact" class="spacer">
-<!--Contact Starts-->
+<!--iniciando sección de contacto-->
 <div class="container contactform center">
+<!-- Añadiendo tema a sección -->
 <h2 class="text-center  wowload fadeInUp">Contáctanos.</h2>
   <div class="row wowload fadeInLeftBig">      
-      <div class="col-sm-6 col-sm-offset-3 col-xs-12">      
+      <div class="col-sm-6 col-sm-offset-3 col-xs-12"> 
+      <!-- Aplicando formato a el formulario -->     
         <input type="text" placeholder="Nombres">
         <input type="text" placeholder="Apellidos">
         <input type="text" placeholder="Compañia">
@@ -142,32 +170,32 @@
   </div>
 </div>
 </div>
-<!--Contact Ends-->
+<!--Contacto fonalizado-->
   <?php
     include 'app/inc/footer.php';
   ?>
-<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<!-- La biblioteca de la Galería de imágenes Bootstrap, debe ser un elemento secundario del body -->
 <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
-    <!-- The container for the modal slides -->
+    <!-- iniciando el contenedor -->
     <div class="slides"></div>
-    <!-- Controls for the borderless lightbox -->
+<!-- controlando las caracteristicas del contenedor -->
     <h3 class="title">Title</h3>
     <a class="prev">‹</a>
     <a class="next">›</a>
     <a class="close">×</a>
-    <!-- The modal dialog, which will be used to wrap the lightbox content -->    
+    <!-- modelo del dialogo, el cual tendra el contenedor -->     
 </div>
-<!-- jquery -->
+<!-- recursos de jquery -->
 <script src="assets/jquery.js"></script>
 <!-- wow script -->
 <script src="assets/wow/wow.min.js"></script>
-<!-- boostrap -->
+<!-- haciendo uso de la biblioteca boostrap para el desarrollo de animaciones -->
 <script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
-<!-- jquery mobile -->
+<!-- animaciones de jquery mobile -->
 <script src="assets/mobile/touchSwipe.min.js"></script>
 <script src="assets/respond/respond.js"></script>
-<!-- gallery -->
+<!-- requiriendo recursos de gallery -->
 <script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
-<!-- custom script -->
+<!-- recursos de script.js -->
 <script src="assets/script.js"></script>
 </body>
